d7ef7dd721b8d3618f8d4a0c14a1456449f7aff9
Fabio <giacomellifabio.74@gmail.com>
2026-09-10 21:48:05 +0200

    Add files via upload

---
+++ 003_VS/MeshCoreTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+using Microsoft.Maui;
+using Foundation;
+
+namespace MeshCoreTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ 003_VS/MeshCoreTracker/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui;
+using Microsoft.UI.Xaml;
+
+namespace MeshCoreTracker.WinUI;
+
+public partial class App : MauiWinUIApplication
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
